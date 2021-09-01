fa3d8de6409a4ef7b55635d33df18a78444042b6
George <goshatravin@gmail.com>
2021-09-01 17:46:45 +0300

    finish first step

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+    .option('-V, --version', 'output the version number')
+    .description('Compares two configuration files and shows a difference.')
+
+program.parse();
+
