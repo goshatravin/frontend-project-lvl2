f68345446a79d347af0c3b536bf768397dbe9df6
George <goshatravin@gmail.com>
2022-08-30 17:19:33 +0300

    step 2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,8 +5,10 @@ import  { Command } from 'commander';
 const program = new Command();
 
 program
-    .name('-h')
+    .name('-h', )
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
 
 program.parse();
