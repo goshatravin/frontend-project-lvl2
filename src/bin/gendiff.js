20617630f22715f5b5e15392496844a193264ac4
George <goshatravin@gmail.com>
2022-09-07 10:38:42 +0300

    play with files

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 
 import  { Command } from 'commander';
+import { readFileSync } from 'fs';
+import path from 'path';
+import process from 'process';
 
 const program = new Command();
 
 program
-    .name('-h', )
+    .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
+    .argument('<filepath1> <filepath2>')
+    .action((data) => {
+        console.log('DATA',data);
+        const path1 = path.resolve(`${process.cwd()}/src/data`, data);
+        const kek = readFileSync( path1 );
+        console.log('123',JSON.parse(kek));
+    });
 
 program.parse();
